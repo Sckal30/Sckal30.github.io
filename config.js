e7aa11dd3614b5436f72959d479a55f4eaed1451
Sckal30 <kblkira@gmail.com>
2026-06-27 12:59:29 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// PEGA AQUÍ LA URL /exec DE TU IMPLEMENTACIÓN DE APPS SCRIPT.
+// Ejemplo: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
+window.CONFIG_ACCESO = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbwL0jsshCWRymA0lwfs_U-CO7mcYL9kbfxrIcmIlaTA3dRN6-ImDDKE-6aiFaoEr2u6/exec'
+};
