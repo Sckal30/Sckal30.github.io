6b73e74c086fcb1c243cd627ecbdb4fe359863c4
Sckal30 <kblkira@gmail.com>
2026-07-09 15:03:15 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // PEGA AQUÍ LA URL /exec DE TU IMPLEMENTACIÓN DE APPS SCRIPT.
 // Ejemplo: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
 window.CONFIG_ACCESO = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyzKqxj3xy3o-45CibX9Kmq9-T1QebuZwR-qZlqE4VJy76Hujy_vNwv0ugILIlRRLC8/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbyNEZaY_S-YDFd_ZlLL7K1Vu_mJhE2l1N0648K2kpFmjvg7R1GVYOnjBpDEzwW2k4pH/exec"
 };
